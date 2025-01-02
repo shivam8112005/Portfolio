08f7676042b046aea7c13e340836f8626bc95b8c
shivam shukla <shivamshukla8112005@gmail.com>
2025-01-02 17:56:51 +0530

    setup complete project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import Hero from './componenets/Hero'
-function App() {
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./componenets/Benefits";
+import Collaboration from "./componenets/Collaboration";
+import Footer from "./componenets/Footer";
+import Header from "./componenets/Header";
+import Hero from "./componenets/Hero";
+import Pricing from "./componenets/Pricing";
+import Roadmap from "./componenets/Roadmap";
+import Services from "./componenets/Services";
 
-
+const App = () => {
   return (
     <>
-      <Hero/>
-    </>
-  )
-}
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
 
-export default App
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
